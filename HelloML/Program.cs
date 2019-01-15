69a9c5696ab35be09c90e0c55bfad9ede61ef771
Hyuk Kim <bbindae@hotmail.com>
2019-01-14 18:50:43 -0800

    adding a solution

---
+++ HelloML/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
